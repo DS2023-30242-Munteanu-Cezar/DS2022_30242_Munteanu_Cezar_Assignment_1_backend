4e9aa4cbf5feb01d5ea8ea783faa270260c02aec
MCezarLucian <guzar107@gmail.com>
2022-12-21 19:02:31 +0200

    ip2

--- src/main/java/MunteanuCezar/SD1/rabbitMQ/configuration/WebSocketConfiguration.java
+++ src/main/java/MunteanuCezar/SD1/rabbitMQ/configuration/WebSocketConfiguration.java
@@ -1,7 +1,8 @@
 package MunteanuCezar.SD1.rabbitMQ.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -17,13 +18,13 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
         config.setApplicationDestinationPrefixes("/app");
     }
 
-    @Value("${server.address}")
-    private String serverAddress;
+    @Autowired
+    Environment environment;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-message")
-                .setAllowedOrigins( serverAddress + ":80")
+                .setAllowedOrigins(environment.getProperty("server.address") + ":80")
                 .withSockJS();
     }
 
